62544c61b75fd9bf4aa6e2889cb6230e047b12e2
Hewen <howardwu2010@gmail.com>
2025-08-26 15:02:55 +0800

    perf: 删除Robot中的AddVisionMeasurement，更新PathplannerLib库到2025.2.7

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,34 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
-#include "LimelightHelpers.h"
 
 #include <frc2/command/CommandScheduler.h>
 
 Robot::Robot() {}
 
-void Robot::RobotPeriodic() {
+void Robot::RobotPeriodic()
+{
   frc2::CommandScheduler::GetInstance().Run();
-
-  /*
-   * This example of adding Limelight is very simple and may not be sufficient for on-field use.
-   * Users typically need to provide a standard deviation that scales with the distance to target
-   * and changes with number of tags available.
-   *
-   * This example is sufficient to show that vision integration is possible, though exact implementation
-   * of how to use vision should be tuned per-robot and to the team's specification.
-   */
-  if (kUseLimelight) {
-    auto const driveState = m_container.drivetrain.GetState();
-    auto const heading = driveState.Pose.Rotation().Degrees();
-    auto const omega = driveState.Speeds.omega;
-
-    LimelightHelpers::SetRobotOrientation("limelight", heading.value(), 0, 0, 0, 0, 0);
-    auto llMeasurement = LimelightHelpers::getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
-    if (llMeasurement && llMeasurement->tagCount > 0 && units::math::abs(omega) < 2_tps) {
-      m_container.drivetrain.AddVisionMeasurement(llMeasurement->pose, llMeasurement->timestampSeconds);
-    }
-  }
 }
 
 void Robot::DisabledInit() {}
@@ -39,10 +19,12 @@ void Robot::DisabledPeriodic() {}
 
 void Robot::DisabledExit() {}
 
-void Robot::AutonomousInit() {
+void Robot::AutonomousInit()
+{
   m_autonomousCommand = m_container.GetAutonomousCommand();
 
-  if (m_autonomousCommand) {
+  if (m_autonomousCommand)
+  {
     m_autonomousCommand->Schedule();
   }
 }
@@ -51,8 +33,10 @@ void Robot::AutonomousPeriodic() {}
 
 void Robot::AutonomousExit() {}
 
-void Robot::TeleopInit() {
-  if (m_autonomousCommand) {
+void Robot::TeleopInit()
+{
+  if (m_autonomousCommand)
+  {
     m_autonomousCommand->Cancel();
   }
 }
@@ -61,7 +45,8 @@ void Robot::TeleopPeriodic() {}
 
 void Robot::TeleopExit() {}
 
-void Robot::TestInit() {
+void Robot::TestInit()
+{
   frc2::CommandScheduler::GetInstance().CancelAll();
 }
 
@@ -70,7 +55,8 @@ void Robot::TestPeriodic() {}
 void Robot::TestExit() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main() {
+int main()
+{
   return frc::StartRobot<Robot>();
 }
 #endif
--- src/main/cpp/subsystems/CommandSwerveDrivetrain.cpp
+++ src/main/cpp/subsystems/CommandSwerveDrivetrain.cpp
@@ -10,28 +10,31 @@ void CommandSwerveDrivetrain::ConfigureAutoBuilder()
     auto config = pathplanner::RobotConfig::fromGUISettings();
     pathplanner::AutoBuilder::configure(
         // Supplier of current robot pose
-        [this] { return GetState().Pose; },
+        [this]
+        { return GetState().Pose; },
         // Consumer for seeding pose against auto
-        [this](frc::Pose2d const &pose) { return ResetPose(pose); },
+        [this](frc::Pose2d const &pose)
+        { return ResetPose(pose); },
         // Supplier of current robot speeds
-        [this] { return GetState().Speeds; },
+        [this]
+        { return GetState().Speeds; },
         // Consumer of ChassisSpeeds and feedforwards to drive the robot
-        [this](frc::ChassisSpeeds const &speeds, pathplanner::DriveFeedforwards const &feedforwards) {
+        [this](frc::ChassisSpeeds const &speeds, pathplanner::DriveFeedforwards const &feedforwards)
+        {
             return SetControl(
                 m_pathApplyRobotSpeeds.WithSpeeds(speeds)
                     .WithWheelForceFeedforwardsX(feedforwards.robotRelativeForcesX)
-                    .WithWheelForceFeedforwardsY(feedforwards.robotRelativeForcesY)
-            );
+                    .WithWheelForceFeedforwardsY(feedforwards.robotRelativeForcesY));
         },
         std::make_shared<pathplanner::PPHolonomicDriveController>(
             // PID constants for translation
             pathplanner::PIDConstants{10.0, 0.0, 0.0},
             // PID constants for rotation
-            pathplanner::PIDConstants{7.0, 0.0, 0.0}
-        ),
+            pathplanner::PIDConstants{7.0, 0.0, 0.0}),
         std::move(config),
         // Assume the path needs to be flipped for Red vs Blue, this is normally the case
-        [] {
+        []
+        {
             auto const alliance = frc::DriverStation::GetAlliance().value_or(frc::DriverStation::Alliance::kBlue);
             return alliance == frc::DriverStation::Alliance::kRed;
         },
@@ -48,29 +51,16 @@ void CommandSwerveDrivetrain::Periodic()
      * Otherwise, only check and apply the operator perspective if the DS is disabled.
      * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
      */
-    if (!m_hasAppliedOperatorPerspective || frc::DriverStation::IsDisabled()) {
+    if (!m_hasAppliedOperatorPerspective || frc::DriverStation::IsDisabled())
+    {
         auto const allianceColor = frc::DriverStation::GetAlliance();
-        if (allianceColor) {
+        if (allianceColor)
+        {
             SetOperatorPerspectiveForward(
                 *allianceColor == frc::DriverStation::Alliance::kRed
                     ? kRedAlliancePerspectiveRotation
-                    : kBlueAlliancePerspectiveRotation
-            );
+                    : kBlueAlliancePerspectiveRotation);
             m_hasAppliedOperatorPerspective = true;
         }
     }
-}
-
-void CommandSwerveDrivetrain::StartSimThread()
-{
-    m_lastSimTime = utils::GetCurrentTime();
-    m_simNotifier = std::make_unique<frc::Notifier>([this] {
-        units::second_t const currentTime = utils::GetCurrentTime();
-        auto const deltaTime = currentTime - m_lastSimTime;
-        m_lastSimTime = currentTime;
-
-        /* use the measured time delta, get battery voltage from WPILib */
-        UpdateSimState(deltaTime, frc::RobotController::GetBatteryVoltage());
-    });
-    m_simNotifier->StartPeriodic(kSimLoopPeriod);
 }
