e48b29eaecdf453704af25837345f7086430b7f1
nahco3bc <nahco3bc@163.com>
2026-01-25 10:41:23 +0800

    Add shooter subsystem in RobotContainer.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -7,88 +7,81 @@
 #include <frc/RobotBase.h>
 #include "simulation/SimulationManager.h"
 
-RobotContainer::RobotContainer() : autoAlign(&drivetrain, &joystick), localization(&drivetrain), gpDetection(&drivetrain)
+RobotContainer::RobotContainer()
+  : autoAlign(&drivetrain, &joystick), localization(&drivetrain), gpDetection(&drivetrain), shooter(joystick)
 {
-    autoChooser = pathplanner::AutoBuilder::buildAutoChooser("Tests");
-    frc::SmartDashboard::PutData("Auto Mode", &autoChooser);
+  autoChooser = pathplanner::AutoBuilder::buildAutoChooser("Tests");
+  frc::SmartDashboard::PutData("Auto Mode", &autoChooser);
 
-    ConfigureBindings();
+  ConfigureBindings();
 }
 
 void RobotContainer::ConfigureBindings()
 {
-    // Note that X is defined as forward according to WPILib convention,
-    // and Y is defined as to the left according to WPILib convention.
-    drivetrain.SetDefaultCommand(
-        // Drivetrain will execute this command periodically
-        drivetrain.ApplyRequest([this]() -> auto &&
-                                {
-                                    return drive.WithVelocityX(-joystick.GetLeftY() * MaxSpeed)      // Drive forward with negative Y (forward)
-                                        .WithVelocityY(-joystick.GetLeftX() * MaxSpeed)              // Drive left with negative X (left)
-                                        .WithRotationalRate(-joystick.GetRightX() * MaxAngularRate); // Drive counterclockwise with negative X (left)
-                                }));
+  // Note that X is defined as forward according to WPILib convention,
+  // and Y is defined as to the left according to WPILib convention.
+  drivetrain.SetDefaultCommand(
+      // Drivetrain will execute this command periodically
+      drivetrain.ApplyRequest([this]() -> auto&& {
+        return drive
+            .WithVelocityX(-joystick.GetLeftY() * MaxSpeed)  // Drive forward with negative Y (forward)
+            .WithVelocityY(-joystick.GetLeftX() * MaxSpeed)  // Drive left with negative X (left)
+            .WithRotationalRate(-joystick.GetRightX() *
+                                MaxAngularRate);  // Drive counterclockwise with negative X (left)
+      }));
 
-    // Idle while the robot is disabled. This ensures the configured
-    // neutral mode is applied to the drive motors while disabled.
-    frc2::RobotModeTriggers::Disabled().WhileTrue(
-        drivetrain.ApplyRequest([]
-                                { return swerve::requests::Idle{}; })
-            .IgnoringDisable(true));
+  // Idle while the robot is disabled. This ensures the configured
+  // neutral mode is applied to the drive motors while disabled.
+  frc2::RobotModeTriggers::Disabled().WhileTrue(
+      drivetrain.ApplyRequest([] { return swerve::requests::Idle{}; }).IgnoringDisable(true));
 
-    // reset the field-centric heading on left bumper press
-    joystick.Start().OnTrue(drivetrain.RunOnce([this]
-                                               { drivetrain.SeedFieldCentric(); }));
+  // reset the field-centric heading on left bumper press
+  joystick.Start().OnTrue(drivetrain.RunOnce([this] { drivetrain.SeedFieldCentric(); }));
 
-    joystick.LeftBumper()
-        .OnTrue(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::LEFT);
-                                   pid_align_command->Schedule(); }))
-        .OnFalse(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   if (pid_align_command && pid_align_command->IsScheduled())
-                                   {
-                                       pid_align_command->Cancel();
-                                   }
-                                   pid_align_command.reset();
-                                   autoAlign.reset(); }));
+  joystick.LeftBumper()
+      .OnTrue(frc2::cmd::RunOnce([this] {
+        pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::LEFT);
+        pid_align_command->Schedule();
+      }))
+      .OnFalse(frc2::cmd::RunOnce([this] {
+        if (pid_align_command && pid_align_command->IsScheduled())
+        {
+          pid_align_command->Cancel();
+        }
+        pid_align_command.reset();
+        autoAlign.reset();
+      }));
 
-    joystick.RightBumper()
-        .OnTrue(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::RIGHT);
-                                   pid_align_command->Schedule(); }))
-        .OnFalse(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   if (pid_align_command && pid_align_command->IsScheduled())
-                                   {
-                                       pid_align_command->Cancel();
-                                   }
-                                   pid_align_command.reset();
-                                   autoAlign.reset(); }));
+  joystick.RightBumper()
+      .OnTrue(frc2::cmd::RunOnce([this] {
+        pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::RIGHT);
+        pid_align_command->Schedule();
+      }))
+      .OnFalse(frc2::cmd::RunOnce([this] {
+        if (pid_align_command && pid_align_command->IsScheduled())
+        {
+          pid_align_command->Cancel();
+        }
+        pid_align_command.reset();
+        autoAlign.reset();
+      }));
 
-    joystick.POVDown()
-        .OnTrue(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::CENTER);
-                                   pid_align_command->Schedule(); }))
-        .OnFalse(
-            frc2::cmd::RunOnce([this]
-                               {
-                                   if (pid_align_command && pid_align_command->IsScheduled())
-                                   {
-                                       pid_align_command->Cancel();
-                                   }
-                                   pid_align_command.reset();
-                                   autoAlign.reset(); }));
+  joystick.POVDown()
+      .OnTrue(frc2::cmd::RunOnce([this] {
+        pid_align_command = autoAlign.PIDAlignCommand(subsystems::AutoAlignSubsystem::Position::CENTER);
+        pid_align_command->Schedule();
+      }))
+      .OnFalse(frc2::cmd::RunOnce([this] {
+        if (pid_align_command && pid_align_command->IsScheduled())
+        {
+          pid_align_command->Cancel();
+        }
+        pid_align_command.reset();
+        autoAlign.reset();
+      }));
 }
 
-frc2::Command *RobotContainer::GetAutonomousCommand()
+frc2::Command* RobotContainer::GetAutonomousCommand()
 {
-    return autoChooser.GetSelected();
+  return autoChooser.GetSelected();
 }
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -8,46 +8,49 @@
 #include "Telemetry.h"
 #include "subsystems/AutoAlignSubsystem.h"
 #include "subsystems/GPDetection.h"
+#include "subsystems/ShooterSubsystem.h"
 
 class RobotContainer
 {
 private:
-    units::meters_per_second_t MaxSpeed = TunerConstants::kSpeedAt12Volts; // kSpeedAt12Volts desired top speed
-    units::radians_per_second_t MaxAngularRate = 0.75_tps;                 // 3/4 of a rotation per second max angular velocity
+  units::meters_per_second_t MaxSpeed = TunerConstants::kSpeedAt12Volts;  // kSpeedAt12Volts desired top speed
+  units::radians_per_second_t MaxAngularRate = 0.75_tps;  // 3/4 of a rotation per second max angular velocity
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    swerve::requests::FieldCentric drive = swerve::requests::FieldCentric{}
-                                               .WithDeadband(MaxSpeed * 0.1)
-                                               .WithRotationalDeadband(MaxAngularRate * 0.1)                     // Add a 10% deadband
-                                               .WithDriveRequestType(swerve::DriveRequestType::OpenLoopVoltage); // Use open-loop control for drive motors
-    swerve::requests::SwerveDriveBrake brake{};
-    swerve::requests::PointWheelsAt point{};
-    swerve::requests::RobotCentric forwardStraight = swerve::requests::RobotCentric{}
-                                                         .WithDriveRequestType(swerve::DriveRequestType::OpenLoopVoltage);
+  /* Setting up bindings for necessary control of the swerve drive platform */
+  swerve::requests::FieldCentric drive =
+      swerve::requests::FieldCentric{}
+          .WithDeadband(MaxSpeed * 0.1)
+          .WithRotationalDeadband(MaxAngularRate * 0.1)                      // Add a 10% deadband
+          .WithDriveRequestType(swerve::DriveRequestType::OpenLoopVoltage);  // Use open-loop control for drive motors
+  swerve::requests::SwerveDriveBrake brake{};
+  swerve::requests::PointWheelsAt point{};
+  swerve::requests::RobotCentric forwardStraight =
+      swerve::requests::RobotCentric{}.WithDriveRequestType(swerve::DriveRequestType::OpenLoopVoltage);
 
-    /* Note: This must be constructed before the drivetrain, otherwise we need to
-     *       define a destructor to un-register the telemetry from the drivetrain */
-    Telemetry logger{MaxSpeed};
+  /* Note: This must be constructed before the drivetrain, otherwise we need to
+   *       define a destructor to un-register the telemetry from the drivetrain */
+  Telemetry logger{ MaxSpeed };
 
-    frc2::CommandXboxController joystick{0};
-
-public:
-    subsystems::CommandSwerveDrivetrain drivetrain{TunerConstants::CreateDrivetrain()};
-    subsystems::AutoAlignSubsystem autoAlign;
-    subsystems::LocalizationSubsystem localization;
-    subsystems::GPDetection gpDetection;
-
-private:
-    /* Path follower */
-    frc::SendableChooser<frc2::Command *> autoChooser;
-
-    std::optional<frc2::CommandPtr> pid_align_command; // 当前正在执行的PID对齐Command
+  frc2::CommandXboxController joystick{ 0 };
 
 public:
-    RobotContainer();
-
-    frc2::Command *GetAutonomousCommand();
+  subsystems::CommandSwerveDrivetrain drivetrain{ TunerConstants::CreateDrivetrain() };
+  subsystems::AutoAlignSubsystem autoAlign;
+  subsystems::LocalizationSubsystem localization;
+  subsystems::GPDetection gpDetection;
+  subsystems::ShooterSubsystem shooter;
 
 private:
-    void ConfigureBindings();
+  /* Path follower */
+  frc::SendableChooser<frc2::Command*> autoChooser;
+
+  std::optional<frc2::CommandPtr> pid_align_command;  // 当前正在执行的PID对齐Command
+
+public:
+  RobotContainer();
+
+  frc2::Command* GetAutonomousCommand();
+
+private:
+  void ConfigureBindings();
 };
