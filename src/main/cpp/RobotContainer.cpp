1c379a36da384524c8676d8bbf0941791460cbc2
Tozy <958339878@qq.com>
2026-04-01 12:23:50 +0800

    start复位加入storage,weidai复位 LLLeft-up高度改为0.146

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -106,9 +106,12 @@ drivetrain.SetDefaultCommand(
   joystick.Start().OnTrue(
     frc2::cmd::Sequence(
     complexcommand.StartStorageCommand(),
-    frc2::cmd::WaitUntil([this]{return weidaiSub.GetStorageNormPosition()>0.7;}),
+    frc2::cmd::WaitUntil([this] { return weidaiSub.GetStorageNormPosition() > 0.7; }),
     complexcommand.GroundintakeresetCommand(),
     groundIntakeSub.SetPitchNormPositionCommandPtr(0.03),
+    frc2::cmd::WaitUntil([this] { return groundIntakeSub.GetPitchNormPosition() <= 0.08; })
+        .WithTimeout(units::second_t{1.0}),
+    complexcommand.CloseStorageCommand(),
     frc2::cmd::RunOnce([this]{ground_intake_prepared_=false;})
   ));
 
